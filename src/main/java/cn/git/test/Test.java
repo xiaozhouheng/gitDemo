4303e27382d5823b5feb1586c85bd991d5058e3f
zhouheng <17633720828@163.com>
2020-04-15 13:52:05 +0800

    周衡完成的新增功能

--- src/main/java/cn/git/test/Test.java
+++ src/main/java/cn/git/test/Test.java
@@ -6,6 +6,7 @@ public class Test {
         System.out.println("周衡分支提交");
         System.out.println("主分支提交");
         System.out.println("zhouheng2分支提交");
+        System.out.println("zhouheng完成了新增功能");
     }
 
 }
